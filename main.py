07326cc8b64e809697e65fed88379718971b8303
20241094010014 <pedroheitorrdac@gmail.com>
2026-05-29 09:34:07 -0300

    fix: hora errada na hora do salvamento

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 from contextlib import asynccontextmanager
+from zoneinfo import ZoneInfo
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -9,6 +11,11 @@ from src.web.api.impression_router import router as impression_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    os.environ["TZ"] = "America/Fortaleza"
+
+    if hasattr(os, "tzset"):
+        os.tzset()
+
     create_tables()
 
 app = FastAPI(
